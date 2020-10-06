b7069f949cb998c10948f77d4946ab42aac2750d
filalex11 <filalexant@yandex.ru>
2020-10-07 00:18:41 +0300

    test1

---
+++ LW_0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a = 0, b = 0, c = 0;
+    printf("Input a : ");
+    scanf("%d",&a);
+    printf("Input b : ");
+    scanf("%d",&b);
+    c = a - b;
+    printf("Result : %d",c);
+    return 0;
+}
